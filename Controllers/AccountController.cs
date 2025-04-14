58661de38a09c2f97f62c2157e988684ab9ef976
Davit Lomsadze <lomsadze741@gmail.com>
2025-04-14 23:41:51 +0400

    Registration

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SkillTrade.Dtos.Account;
+using SkillTrade.Identity;
+
+namespace SkillTrade.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<UserIdentity> _userManager;
+
+        public AccountController(UserManager<UserIdentity> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                UserIdentity appUser = new UserIdentity
+                {
+                    UserName = registerDto.Username,
+                    Email = registerDto.Email
+                };
+
+                var createUser = await _userManager.CreateAsync(appUser, registerDto.Password);
+
+                if (createUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(appUser, "user");
+
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok("User created");
+                    }
+                    else
+                    {
+                        return StatusCode(500, roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return StatusCode(500, createUser.Errors);
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
+    }
+}

bea131a56db5430dec43edd7926fcf5b18f5ceb8
Davit Lomsadze <lomsadze741@gmail.com>
2025-04-20 21:19:15 +0400

    Profilerepo with interface

---
+++ Interfaces/IProfileInterface.cs
@@ -0,0 +1,15 @@
+﻿using SkillTrade.Dtos.Create;
+using SkillTrade.Dtos.Update;
+using SkillTrade.Entities;
+
+namespace SkillTrade.Interfaces
+{
+    public interface IProfileInterface
+    {
+        public Task<List<Profile>> GetAllProfilesAsync();
+        public Task<Profile?> GetProfileByIdAsync(int id);
+        public Task CreateProfileAsync(Profile userProfile);
+        public Task UpdateProfileAsync(Profile userProfile, Profile updatedUserProfile);
+        public Task DeleteProfileAsync(int id);
+    }
+}
---
+++ Repositories/ProfileRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillTrade.Data;
+using SkillTrade.Entities;
+using SkillTrade.Interfaces;
+
+namespace SkillTrade.Repositories
+{
+    public class ProfileRepository : IProfileInterface
+    {
+        private readonly SkillTradeDbContext _context;
+
+        public ProfileRepository(SkillTradeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Profile>> GetAllProfilesAsync()
+        {
+            List<Profile> profiles = await _context.Profiles.ToListAsync();
+
+            return profiles;
+        }
+
+        public async Task<Profile?> GetProfileByIdAsync(int id)
+        {
+            Profile? profile = await _context.Profiles.FindAsync(id);
+
+            return profile;
+        }
+
+        public async Task CreateProfileAsync(Profile userProfile)
+        {
+            await _context.Profiles.AddAsync(userProfile);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateProfileAsync(Profile userProfile, Profile updatedUserProfile)
+        {
+            _context.Profiles.Entry(userProfile).CurrentValues.SetValues(updatedUserProfile);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProfileAsync(int id)
+        {
+           await _context.Profiles.Where(p => p.Id == id).ExecuteDeleteAsync();
+        }
+    }
+}

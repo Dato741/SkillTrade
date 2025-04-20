08762bd6ccc6b72584db37a50ae6a51453cd8ad5
Davit Lomsadze <lomsadze741@gmail.com>
2025-04-20 22:10:45 +0400

    Profile mapping

---
+++ Mapping/ProfileMapping.cs
@@ -0,0 +1,30 @@
+﻿using SkillTrade.Dtos.Create;
+using SkillTrade.Dtos.ToClient;
+using SkillTrade.Entities;
+
+namespace SkillTrade.Mapping
+{
+    public static class ProfileMapping
+    {
+        public static ProfileDto ToProfileDto(this Profile profile)
+        {
+            return new ProfileDto
+            {
+                Id = profile.Id,
+                Name = profile.Name,
+                LastName = profile.LastName,
+                ServiceList = profile.ServiceList,
+                Bookings = profile.Bookings,
+            };
+        }
+
+        public static Profile ToProfileEntity(this CreateProfileDto profile)
+        {
+            return new Profile
+            {
+                Name = profile.Name,
+                LastName = profile.LastName
+            };
+        }
+    }
+}

fd09f547e70e96beeba8bfe284db96156944ae9f
Davit Lomsadze <lomsadze741@gmail.com>
2025-04-22 20:04:27 +0400

    Booking Dto with mapping

---
+++ Dtos/ToClient/BookingDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillTrade.Dtos.ToClient
+{
+    public class BookingDto
+    {
+        public int BookingId { get; set; }
+        public string ServiceName { get; set; } = string.Empty;
+        public string ServiceDescription { get; set; } = string.Empty;
+        public int PayOffered { get; set; }
+    }
+}
---
+++ Mapping/BookingMapping.cs
@@ -0,0 +1,19 @@
+﻿using SkillTrade.Dtos.ToClient;
+using SkillTrade.Entities;
+
+namespace SkillTrade.Mapping
+{
+    public static class BookingMapping
+    {
+        public static BookingDto ToBookingDto(this Booking booking)
+        {
+            return new BookingDto
+            {
+                BookingId = booking.Id,
+                ServiceName = booking.Service!.Name,
+                ServiceDescription = booking.Service!.Description,
+                PayOffered = booking.Service!.PayOffered
+            };
+        }
+    }
+}
